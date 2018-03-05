8ad47c8f9e020821256c9e0db9fa8177d3f22a02
thorlord <thorah2908@gmail.com>
2018-03-05 14:47:15 +0100

    CRA installed

--- src/index.js
+++ src/index.js
@@ -1,36 +1,8 @@
-import React, { Component } from 'react'
-import { render } from 'react-dom'
-import { BrowserRouter as Router, Route, } from 'react-router-dom'
-import registerServiceWorker from './registerServiceWorker'
-import './index.css'
-import App from 'App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
-class AppRouter extends Component {
-	render() {
-		return (
-			<Router>
-				<div>
-					<Route path='/' component={App} />
-				</div>
-			</Router>
-		)
-	}
-}
-export default AppRouter
-
-render(
-	<AppRouter />,
-	document.getElementById('root')
-)
-
-if (module.hot) {
-	module.hot.accept('./index'.default, () => {
-		const NextApp = require('./index').default
-		render(
-			<NextApp />,
-			document.getElementById('root')
-		)
-	})
-}
-
-registerServiceWorker()
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
