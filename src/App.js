a669252aa08ff90830b1542ebb19719e66483627
thorlord <thorah2908@gmail.com>
2018-03-06 15:12:38 +0100

    saved changes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-/*      
+   
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
@@ -15,9 +15,7 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
-*/
-      <div>
-      </div>
+
     );
   }
 }
