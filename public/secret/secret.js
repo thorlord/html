36f0403860dfbccd434d2fb4bed6ce7408f88f96
Christian Broberg <cb@webhouse.dk>
2018-03-07 16:46:29 +0100

    Secrets revealed 1

--- public/secret/secret.js
+++ public/secret/secret.js
@@ -1,1 +1,14 @@
 
+const bulp1 = '1'
+const bulp2 = '2'
+
+function light(sw, bulp) {
+	var pic;
+	if (sw == 0) {
+		pic = "pic_bulboff.gif"
+	} else {
+		pic = "pic_bulbon.gif"
+	}
+	document.getElementById('myImage' + bulp).src = pic;
+}
+
