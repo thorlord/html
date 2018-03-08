366d917b707bb95dbd36216919643d0b2c180756
Christian Broberg <cb@webhouse.dk>
2018-03-08 21:57:31 +0100

    Hello Dolly

--- public/home/memes.js
+++ public/home/memes.js
@@ -12,6 +12,20 @@ const imageMemes = [
 	'https://heavyeditorial.files.wordpress.com/2016/11/screen-shot-2016-11-08-at-11-44-23-pm.jpg?quality=65&strip=all&w=780'
 ]
 
+const memes = [
+	'Cheese bags can be hard to open for teenage slackers',
+	'Ham is an important engredient in a nerd toast',
+	'Lettuce is too healthy',
+	'Bread is not a color that resembles cyan',
+	'Tomato is the prefered color in most web examples',
+	'Mustard is totally deept fried',
+	'Ketchup is like tomato, pretty bloody',
+	'Pickles is like being in a jam ... pickle',
+	'Thorlord is one deep fried teenage slacker hacker ... ',
+	'When nerds makes memes be like...'
+]
+
+
 // Function named with camelcase - first letter always small, consecutive words uppercased first letter. 
 function randomImageMemes() {
 	return imageMemes[Math.floor(Math.random() * imageMemes.length)];
@@ -19,4 +33,12 @@ function randomImageMemes() {
 
 function displayImageMemes() {
 	document.getElementById("imagememes").src = randomImageMemes()
+}
+
+function randomMemes() {
+	return memes[Math.floor(Math.random() * memes.length)];
+}
+
+function displayMemes(id) {
+	document.getElementById('memes').innerHTML = randomMemes()
 }
