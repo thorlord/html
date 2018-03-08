751b19f09102a332141dced379d976f7993e0aee
thorlord <thorah2908@gmail.com>
2018-03-08 21:57:52 +0100

    hire it comes

--- public/home/memes.js
+++ public/home/memes.js
@@ -9,7 +9,12 @@ const imageMemes = [
 	'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTX0r5EjROqX83Fsy_2iPvfkRhRRw1EkHkIDIVVF2lfwrkVz1GN',
 	'http://www.dumpaday.com/wp-content/uploads/2016/11/119.jpg',
 	'http://i0.kym-cdn.com/photos/images/newsfeed/001/154/781/e7f.jpg',
-	'https://heavyeditorial.files.wordpress.com/2016/11/screen-shot-2016-11-08-at-11-44-23-pm.jpg?quality=65&strip=all&w=780'
+	'https://heavyeditorial.files.wordpress.com/2016/11/screen-shot-2016-11-08-at-11-44-23-pm.jpg?quality=65&strip=all&w=780',
+	'https://i.ytimg.com/vi/9mv17I9mGKw/maxresdefault.jpg',
+	'https://i2.wp.com/picsdownloadz.com/wp-content/uploads/2016/09/their-yard-sign-said-trump-momma-hates-trump-Funny-hilarious-trump-Memes.jpg?resize=600%2C451',
+	'http://www.funnybeing.com/wp-content/uploads/2017/02/Mexicans-Training-For-Donald-Trumps-Wall.png',
+	'https://pics.me.me/donald-trumps-hair-finally-runs-off-to-seek-a-better-12189603.png',
+	''
 ]
 
 // Function named with camelcase - first letter always small, consecutive words uppercased first letter. 
