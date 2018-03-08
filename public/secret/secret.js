0f6d81f4831a6d47a1ff204344e5f850159c6e44
Christian Broberg <cb@webhouse.dk>
2018-03-08 19:07:36 +0100

    Ohhhh jolly

--- public/secret/secret.js
+++ public/secret/secret.js
@@ -20,3 +20,20 @@ function myFunction(p1, p2) {
 	return p1 * p2;              // The function returns the product of p1 and p2
 }
 
+
+const memes = [
+	'Cheese bags can be hard to open for teenage slackers',
+	'Ham is an important engredient in a nerd toast',
+	'Lettuce is too healthy',
+	'Bread is not a color that resembles cyan',
+	'Tomato is the prefered color in most web examples',
+	'Mustard is totally deept fried',
+	'Ketchup is like tomato, pretty bloody',
+	'Pickles is like being in a jam ... pickle',
+	'Thorlord is one deep fried teenage slacker hacker ... '
+]
+
+function randomMemes() {
+	return memes[Math.floor(Math.random() * memes.length)];
+}
+
