da2d0e27d23aadb0f9bdf7ffb4d7c95b0630effb
thorlord <thorah2908@gmail.com>
2018-03-08 22:07:48 +0100

    i am sparta

--- public/home/memes.js
+++ public/home/memes.js
@@ -6,7 +6,6 @@ const imageMemes = [
 	'http://worldwideinterweb.com/wp-content/uploads/2016/03/all-the-memes-in-the-world.jpg',
 	'http://politicalpunchline.com/wp-content/uploads/2017/08/trump-joke-bar.jpg',
 	'https://www.humortimes.com/wp-content/uploads/2017/02/hi-there-donny.jpg',
-	'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTX0r5EjROqX83Fsy_2iPvfkRhRRw1EkHkIDIVVF2lfwrkVz1GN',
 	'http://www.dumpaday.com/wp-content/uploads/2016/11/119.jpg',
 	'http://i0.kym-cdn.com/photos/images/newsfeed/001/154/781/e7f.jpg',
 	'https://heavyeditorial.files.wordpress.com/2016/11/screen-shot-2016-11-08-at-11-44-23-pm.jpg?quality=65&strip=all&w=780',
