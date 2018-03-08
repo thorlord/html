7c3b97de811c7575a3b6017e14b8be30df81ba7a
Christian Broberg <cb@webhouse.dk>
2018-03-08 09:52:07 +0100

    New

--- public/secret/secret.js
+++ public/secret/secret.js
@@ -16,3 +16,7 @@ function hello() {
 	window.alert("Hello! I am an alert box!!")
 }
 
+function myFunction(p1, p2) {
+	return p1 * p2;              // The function returns the product of p1 and p2
+}
+
