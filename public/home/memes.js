05a5e630ebe222856ec4117d4f3b3de5ecda96e7
thorlord <thorah2908@gmail.com>
2018-03-09 14:33:53 +0100

    color change

--- public/home/memes.js
+++ public/home/memes.js
@@ -14,7 +14,20 @@ const imageMemes = [
 	'https://heavyeditorial.files.wordpress.com/2016/11/screen-shot-2016-11-08-at-11-44-23-pm.jpg',
 	'https://i.ytimg.com/vi/9mv17I9mGKw/maxresdefault.jpg',
 	'https://i2.wp.com/picsdownloadz.com/wp-content/uploads/2016/09/their-yard-sign-said-trump-momma-hates-trump-Funny-hilarious-trump-Memes.jpg',
-
+	'https://cdn.pastemagazine.com/www/system/images/photo_albums/trump-memes/large/150714204025-moos-trump-your-cat-meme-erin-00000918-full-169.jpg?',
+	'https://fthmb.tqn.com/hHDgGNVFaGS1ALYY09-NfHiQBYQ=/768x0/filters:no_upscale()/trump-wall-been-there-56dfa04f5f9b5854a9f6c87e.jpg',
+	'https://heavyeditorial.files.wordpress.com/2017/06/cover20.jpg',
+	'http://mojly.com/wp-content/uploads/2017/11/http3A2F2Fwww.fungur.com2Fuploads2F20162F092Ffunniest-Donald-Trump-Hillary-Clinton-Memes-5.jpg',
+	'https://lgbtqnation-assets.s3.amazonaws.com/assets/2017/11/Capture-400x283.jpg',
+	'https://s-media-cache-ak0.pinimg.com/originals/7c/5b/1a/7c5b1a580c2e22072da1fb784b7197bf.jpg',
+	'http://www.funnybeing.com/wp-content/uploads/2017/02/So-You-Are-Saying-Donald-Trump-600x586.jpg',
+	'https://img.buzzfeed.com/buzzfeed-static/static/2017-01/26/15/campaign_images/buzzfeed-prod-fastlane-02/14-memes-mexicans-are-using-to-deal-with-the-wall-2-32271-1485462846-2_dblbig.jpg',
+	'https://us-east-1.tchyn.io/snopes-production/uploads/2015/08/bachmann.jpg',
+	'https://fthmb.tqn.com/hpbraJnJIR0pIFhX9-BIcj1D5O4=/768x0/filters:no_upscale()/trump-hillary-deplorables-57d70f155f9b589b0a3f056b.jpg',
+	'https://racistmemes.com/wp-content/uploads/2017/03/mexican-memes-jokes.jpg',
+	'https://cdn-images-1.medium.com/max/1600/1*_IHjai_aoBoWYi0Bw_IANQ.png',
+	'https://i.imgflip.com/1i01l4.jpg',
+	'http://politicalpunchline.com/wp-content/uploads/2018/01/wall-mexico-pay-stupid.jpg',
 ]
 
 const memes = [
